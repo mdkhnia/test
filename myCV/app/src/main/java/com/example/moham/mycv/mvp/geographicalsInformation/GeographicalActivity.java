725692264106d041719fa9bce91fa30d71e8a184
mdkhnia <mohammadkhnia@gmail.com>
2018-05-12 19:05:28 +0430

    add project

---
+++ myCV/app/src/main/java/com/example/moham/mycv/mvp/geographicalsInformation/GeographicalActivity.java
@@ -0,0 +1,15 @@
+package com.example.moham.mycv.mvp.geographicalsInformation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.moham.mycv.R;
+
+public class GeographicalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geographical);
+    }
+}
---
+++ myCV/app/src/main/java/com/example/moham/mycv/mvp/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.moham.mycv.mvp.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.moham.mycv.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
