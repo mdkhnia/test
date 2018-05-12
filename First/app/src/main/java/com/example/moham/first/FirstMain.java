d5bf29f66b708f0593efbd4eb74c3d5a63a15c40
mdkhnia <mohammadkhnia@gmail.com>
2018-05-12 17:16:43 +0430

    added

---
+++ First/app/src/main/java/com/example/moham/first/FirstMain.java
@@ -0,0 +1,18 @@
+package com.example.moham.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
